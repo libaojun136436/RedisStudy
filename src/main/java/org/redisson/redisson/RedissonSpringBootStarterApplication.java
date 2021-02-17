4cc9af3eddb6ab9c017b1dda35d21db4a22d64dd
libaojun <810136020@qq.com>
2021-02-17 23:06:13 +0800

    提交手写一个redis start

---
+++ src/main/java/org/redisson/redisson/RedissonSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package org.redisson.redisson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedissonSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonSpringBootStarterApplication.class, args);
+    }
+
+}
